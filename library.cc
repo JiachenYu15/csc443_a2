b49683fe0055e598d410cf65099e72ce8024ac46
Yu Jiachen <yujiache@teach.cs.toronto.edu>
2018-11-04 17:25:55 -0500

    Add all the defined types and class to library.h

---
+++ library.cc
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <iostream>
+#include "library.h"
+using namespace std;
+
+
--- library.h
+++ library.h
@@ -1,7 +1,24 @@
+#include <stdio.h>
 #include <vector>
+
+/*
+ * Data strcutre declaration
+ */
+
+/******* Section 2 ********/
 
 typedef const char* V;
 typedef std::vector<V> Record;
+
+/******* Section 3 ********/
+
+typedef struct {
+  void *data;
+  int page_size;
+  int slot_size;
+}Page;
+
+/******* Section 4 ********/
 
 typedef struct {
     FILE *file_ptr;
@@ -15,10 +32,19 @@ typedef struct {
     int slot;
 }RecordID;
 
-typedef struct {
-  void *data;
-  int page_size;
-  int slot_size;
-}Page;
+class RecordIterator{
+    public:
+        RecordIterator(Heapfile *heapfile);
+        Record next();
+        bool hasNext();
+};
 
+/*
+ * Functiton declarations
+ */
 
+/******** Section 2 *******/
+
+/******** Section 3 *******/
+
+/******** Section 4 *******/
