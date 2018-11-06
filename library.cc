9c1a383aae8fe55c8f17540b95bdc0cbcfb4e27e
Yu Jiachen <yujiache@teach.cs.toronto.edu>
2018-11-05 19:33:14 -0500

    Tested heapfunctions.

--- library.cc
+++ library.cc
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
 #include "library.h"
 using namespace std;
@@ -17,9 +18,14 @@ void init_heapfile(Heapfile *heapfile, int page_size, FILE *file){
 PageID alloc_page(Heapfile *heapfile){
     FILE* file_ptr = heapfile->file_ptr;
     int page_size = heapfile->page_size;
-    
-    fseek(file_ptr, page_size, SEEK_END);
-    fputc('\0', file_ptr);
+    PageID pageid;
+
+    fseek(file_ptr, 0, SEEK_END);
+    pageid = ftell(file_ptr)/page_size;
+    char* buffer = (char*)calloc(page_size, 1);
+    fwrite(buffer, page_size, 1, file_ptr);
+    free(buffer);
+    return pageid;
 };
 
 void read_page(Heapfile *heapfile, PageID pid, Page *page){
@@ -27,7 +33,7 @@ void read_page(Heapfile *heapfile, PageID pid, Page *page){
     int page_size = heapfile->page_size;
 
     fseek(file_ptr, page_size*pid, SEEK_SET);
-    fread((void*)page, page_size, 1, file_ptr);
+    fread(page, page_size, 1, file_ptr);
 }
 
 void write_page(Page *page, Heapfile *heapfile, PageID pid){
